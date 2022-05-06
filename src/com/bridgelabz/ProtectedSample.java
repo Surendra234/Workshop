ffd58c32afb19f025d4021820d7aa39a4b2f4c25
Surendra234 <surendramahawar873@gmail.com>
2022-05-06 11:55:18 +0530

    Workshop

---
+++ src/com/bridgelabz/ProtectedSample.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class ProtectedSample extends Parrot {
+
+        void sample() {
+            Parrot p = new Parrot();
+            System.out.println(colour);
+        }
+}
